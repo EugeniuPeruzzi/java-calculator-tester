5dd5bbf39c82c9660371f002045a235237d0fc09
EugeniuPeruzzi <jaka1997@live.it>
2023-11-27 12:29:06 +0100

    first commit

---
+++ src/main/java/org/java/Calculator.java
@@ -0,0 +1,35 @@
+package org.java;
+
+public class Calculator {
+	float num1;
+	float num2;
+	
+	public Calculator(float num1, float num2){
+		setNum1(num1);
+		setNum2(num2);
+	}
+
+	public float getNum1() {
+		return num1;
+	}
+
+	public void setNum1(float num1) {
+		this.num1 = num1;
+	}
+
+	public float getNum2() {
+		return num2;
+	}
+
+	public void setNum2(float num2) {
+		this.num2 = num2;
+	}
+	
+
+	@Override
+	public String toString() {
+		
+		return super.toString();
+	}
+	
+}
