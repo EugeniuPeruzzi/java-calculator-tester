43f07cb2dbcb0daa0acd0f820db4f0f6b94d4e60
EugeniuPeruzzi <jaka1997@live.it>
2023-11-27 14:40:59 +0100

    creata classe ticket

---
+++ src/main/java/org/java/Ticket.java
@@ -0,0 +1,31 @@
+package org.java;
+
+public class Ticket {
+	private int km;
+	private int age;
+	
+	public Ticket(int km, int age) {
+		setKm(km);
+		setAge(age);
+	}
+
+	public int getKm() {
+		return km;
+	}
+
+	public void setKm(int km) {
+		this.km = km;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
+	 
+}
